859f02e61d832838f6c8b96a3788c576115ecfcc
akash marwah <akashmarwah07@gmail.com>
2019-02-08 02:47:03 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to Doodle</title>
+    <link rel="shortcut icon" href="assets/images/doodle.png">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+  </head>
+  <body>
+    <div class="wrapper indexPage">
+   <div class="mainSection">
+      <div class="logoContainer">
+        <img src="assets/images/logo.png" style="width:250px; height:100px;" alt="Doodle">
+      </div>
+
+      <div class="searchContainer">
+        <form class="" action="search.php" method="GET">
+
+          <input type="text" name="term" value="" class="searchBox">
+          <input type="submit" name="" value="Search" class="searchButton">
+
+        </form>
+      </div>
+      <p>
+        
+      </p>
+    </div>
+  </div>
+  </body>
+</html>
---
+++ search.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to Doodle</title>
+    <link rel="shortcut icon" href="assets/images/doodle.png">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+  </head>
+  <body>
+    <div class="wrapper">
+      <div class="header">
+
+        <div class="headerContent">
+          <div class="logoContainer">
+          <a href="index.php">
+            <img src="assets/images/logo.png" style="width:250px; height:100px;" alt="Doodle">
+          </a>
+          </div>
+          <div class="searchContainer">
+            <form class="" action="search.php" method="GET">
+              <div class="searchBarContainer">
+                <input type="text" name="term" value="" class="searchBox">
+                  <button type="button" name="button">
+                    Search
+                  </button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+      </div>
+  </div>
+  </body>
+</html>
