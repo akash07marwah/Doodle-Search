ac83bb8ad26448d9e8e052bf4ec121457c24a5f9
akash marwah <akashmarwah07@gmail.com>
2019-02-12 14:49:30 +0530

    Fifth Commit

--- classes/ddp.php
+++ classes/ddp.php
@@ -1,5 +1,20 @@
 <?php
-class DomDocumentParser{
-  
+class DomDocumentParser {
+
+	private $doc;
+
+	public function __construct($url) {
+
+		$options = array(
+			'http'=>array('method'=>"GET", 'header'=>"User-Agent: doodleBot/0.1\n")
+			);
+		$context = stream_context_create($options);
+
+		$this->doc = new DomDocument();
+		@$this->doc->loadHTML(file_get_contents($url, false, $context));
+	}
+  public function getLinks(){
+    return $this->doc->getElementsByTagName("a");
+  }
 }
- ?>
+?>
